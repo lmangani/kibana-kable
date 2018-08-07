e4c582763dc4edb3ab49b90117d458509aab9581
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-08-07 20:32:14 +0200

    correct logo path

--- public/register_feature.js
+++ public/register_feature.js
@@ -5,7 +5,7 @@ FeatureCatalogueRegistryProvider.register(() => {
     id: 'kable',
     title: 'Kable',
     description: 'Use an expression language to analyze data and visualize the results.',
-    icon: 'plugins/kable/kable_feature.svg',
+    icon: '/plugins/kable/kable_feature.svg',
     path: '/app/kable',
     showOnHomePage: true,
     category: FeatureCatalogueCategory.DATA
